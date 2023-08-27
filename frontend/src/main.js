c9fd2d389dfeccf48a12ea2923ed3cd2945f6ad6
mooyiluo <mooyiluo@outlook.com>
2023-08-27 20:52:28 +0800

    frontend update

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,17 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
 import App from './App.vue'
 import router from './router'
+import { createVuestic } from "vuestic-ui";
+import "vuestic-ui/css";
+import NaiveUI from 'naive-ui'
+import { DatePicker } from 'ant-design-vue';
 
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus).use(createVuestic()).use(NaiveUI).use(DatePicker)
 
 app.mount('#app')
---
+++ frontend/src/views/Coffee.vue
@@ -0,0 +1,43 @@
+<template>
+    <a-card hoverable style="width: 300px">
+      <template #cover>
+        <img
+          alt="example"
+          src := coffee.coffeePic
+        />
+      </template>
+      <template #actions>
+        <setting-outlined key="setting" />
+        <edit-outlined key="edit" />
+        <ellipsis-outlined key="ellipsis" />
+      </template>
+      <a-card-meta title :=coffee.coffeeName>
+        <template #avatar>
+          <a-avatar src :=coffee.coffeeShop />
+        </template>
+      </a-card-meta>
+    </a-card>
+  </template>
+
+<script>
+import request from '@/utils/request.js'
+
+export default{
+    name: 'Coffee',
+    components: {
+
+    },
+    data(){
+        return{
+            coffee: {},
+        }
+    },
+    methods: {
+        load(){
+            request.get("/coffee").then(res => {
+                console.log(res)
+            })
+        }
+    }
+}
+</script>
